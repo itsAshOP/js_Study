e42826b7c34a4005578dc460a2f4c6c73959564f
itsAshOP <guravaayush28@gamil.com>
2024-07-08 23:07:20 +0530

    operator

---
+++ operator.js
@@ -0,0 +1,94 @@
+//arithematic operator
+// let a = 10;
+// let b = 4;
+
+// console.log("a = ", a,"b = ",b);
+
+
+// console.log("a+b= ",a+b);
+// console.log("a-b= ",a-b);
+// console.log("a*b= ",a*b);
+// console.log("a/b= ",a/b);
+// console.log("a%b= ",a%b);
+
+// for ex: 10*10*10*10=10000.
+// console.log("a & b = ", a**b);
+
+/*unary operator
+Increment and Decrement
+postIncrement
+*/
+// a++;
+// console.log("a= ",a++);
+// b--;
+// console.log("b= ",b--);
+
+// //preIncrement
+// --a;
+// console.log("a= ",--a);
+// ++b;
+// console.log("b= ",++b);
+
+// --------------------------------------------------------
+// Assignment operator
+
+//+= -= *= /= **= %=
+// let a = 10;
+// let b = 4;
+
+// console.log("a = ", a,"b = ",b);
+
+// a+=1;
+// console.log("a+=",a)
+// a-=1;
+// console.log("a-=",a);
+// a*=1;
+// console.log("a*= ", a);
+// a/=5;
+// console.log("a/=",a);
+// a**=5;
+// console.log("a**=",a);
+// a%=5;
+// console.log("a%= ",a);
+
+// -----------------------------------------------
+// comparison operator
+
+/* ==(equal to) 
+!=(not equal to) 
+===(striple equal to, it also find that which datatype is there)
+!== (^same^) it a stritker version
+< (greater then)
+> (lesser then)
+<= (that means the number lesser then or equal to)
+>= (that means the number greater then or equal to)
+*/
+
+// let a = 10;
+// let b = "4";
+
+// console.log("a = ", a,"b = ",b);
+
+// console.log("10 == 4 ",a==b);
+// console.log("10 != 4 ",a!=b);
+// console.log("10 === 4 ",a===b);
+// console.log("10 !== 4 ",a!==b);
+// console.log("10 > 4 ",a>b);
+// console.log("10 < 4 ",a<b);
+// console.log("10 <= 4 ",a<=b);
+// console.log("10 >= 4 ",a>=b);
+
+// logical operator
+
+let a = 10;
+let b = "4";
+
+console.log("a = ", a,"b = ",b);
+
+// let cond1 = a > b;
+// let cond2 = a === 10;
+// console.log("cond1 && cond2 = ", cond1&&cond2);
+
+let cond1 = a > b;
+let cond2 = a === 10;
+console.log("cond1 || cond2 = ", cond1||cond2);
