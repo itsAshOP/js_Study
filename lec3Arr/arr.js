d6ce9a9b4e1480d8f1561b187389dec8f0de5ce7
itsAshOP <guravaayush28@gamil.com>
2024-07-08 13:53:57 +0530

    changes

--- lec3Arr/arr.js
+++ lec3Arr/arr.js
@@ -1,6 +1,13 @@
-let marks=[34,36,55];
-console.log(marks);
-console.log(marks.length);
+// let marks=[34,36,55];
+// console.log(marks);
+// console.log(marks.length);
 
-let heroes=["ram","lakshman","mahadev","hanuman"];
-console.log(heroes);
+// let heroes=["ram","lakshman","mahadev","hanuman"];
+// console.log(heroes);
+
+let fruit = ["mango","banana","jackfruit"];
+//  fruit[1] = "apple"; this is to replace a element
+// fruit[3] = "pear"; this is to add a element 
+
+
+alert(fruit[fruit.length-1]);
